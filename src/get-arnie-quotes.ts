7d1fc88eb39b2cb56e3ccc078863906d1b989a16
lukelovekin <lukelovekin@gmail.com>
2022-10-18 18:14:52 +1000

    solution

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,21 @@
 import { httpGet } from './mock-http-interface';
 
-// TODO define this type properly
-type TResult = any;
+type TResult = { "FAILURE": string } | { "Arnie Quote": string }
+type GetResult = { status: number, body: string}
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
   
-  return [];
+  const promises: Promise<GetResult>[] = urls.map((url) => {
+    // errors already caught
+    return httpGet(url)
+  })
+
+  // Would normally avoid promise.all as much as possible 
+  const allQuotes = await Promise.all(promises).then((resp) => {
+    return resp.map(x => x.status !== 200
+      ? { 'FAILURE': 'Your request has been terminated' }
+      : { 'Arnie Quote': JSON.parse(x.body).message })
+  })
+
+  return allQuotes
 };
